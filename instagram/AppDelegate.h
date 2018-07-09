597bab151c5aae362b66454d8d5dce38ce736bf5
Joel Gutierrez <joelgutz@fb.com>
2018-07-09 14:33:46 -0700

    Initial Commit

---
+++ instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagram
+//
+//  Created by Joel Gutierrez on 7/9/18.
+//  Copyright © 2018 Joel Gutierrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
